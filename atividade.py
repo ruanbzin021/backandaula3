3003ed8a4889c0533d56104cd522347182ef5299
RUAN BARROS DE SOUSA <ruan.sousa@aluno.senac.rio>
2025-09-24 18:52:54 -0300

    atividade

--- atividade.py
+++ atividade.py
@@ -1,1 +1,18 @@
-print("boa")
+class Carro:
+    rodas = 4
+    
+    def __init__(self, marca, ano):
+        self.marca = marca
+        self.ano = ano
+    
+    def ligar(self):
+        print(f"O carro {self.marca} ({self.ano}) está ligado.")
+        
+    @classmethod
+    def mudar_numero_rodas(cls, novas_rodas):
+        cls.rodas = novas_rodas
+        print(f"Agora todos os carros tem {cls.rodas} rodas.")
+        
+    @staticmethod
+    def calcular_idade(ano_fabricacao, ano_atual):
+        return ano_atual - ano_fabricacao                
